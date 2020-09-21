2ad2b2bd309c981410c1d6abbf96cd19e0727b82
Matt Dancho <mdancho@gmail.com>
2020-09-21 07:11:02 -0400

    usethis - vignettes, data, rmarkdown

---
+++ R/data.R
@@ -0,0 +1,20 @@
+
+# M750 ----
+
+#' The 750th Monthly Time Series used in the M4 Competition
+#'
+#'
+#' @format
+#' A `tibble` with 306 rows and 3 variables:
+#'
+#'  - `id` Factor. Unique series identifier
+#'  - `date` Date. Timestamp information. Monthly format.
+#'  - `value` Numeric. Value at the corresponding timestamp.
+#'
+#' @examples
+#' m750
+#'
+#' @source
+#'  - [M4 Competition Website](https://mofc.unic.ac.cy/m4/)
+#'
+"m750"
