ccc5e1d225d940afe605903942601eb97c11e1ed
Matt Dancho <mdancho@gmail.com>
2021-10-19 13:10:02 -0400

    add global vars

--- R/00_global_variables.R
+++ R/00_global_variables.R
@@ -2,5 +2,5 @@ utils::globalVariables(
     c(".index", ".key", ".value", ".model_id", ".loadings", ".model", ".model_desc",
       ".pred", ".predictions", ".resample_results", ".row_id", ".preds", ".type",
       "n", ".resample_id", ".", "rowid..", "..rowid", "x", "d", "s", "id",
-      "direction", "metric")
+      "direction", "metric", ".error_desc")
 )
